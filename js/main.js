4e08e3f290c7b37d002b0a4593f74229a4c5b7cd
inlancersystem4 <inlancersystem4@gmail.com>
2024-03-22 12:00:29 +0530

    add home new design and more

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     AOS.init();
 
 
-    $(".dropdown .selected-option").click(function() {
+    $(".dropdown .selected-option").click(function () {
         var dropdown = $(this).closest('.dropdown');
         dropdown.find(".dropdown-list").slideToggle();
         dropdown.find(".bi").toggleClass("rotate_180deg");
     });
 
-    $(".dropdown .dropdown-list li").click(function() {
+    $(".dropdown .dropdown-list li").click(function () {
         var selectedOption = $(this).text();
         var dropdown = $(this).closest('.dropdown');
         dropdown.find(".selected-option").html(selectedOption + ' <i class="bi bi-chevron-down"></i>');
@@ -18,37 +18,23 @@ $(document).ready(function() {
     });
 
 
-    $('.testimonial-slider').slick({
-        slidesToShow: 2,
-        slidesToScroll: 2,
+    $('[testimonial-slider]').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        dots: true,
+        dots: false,
         arrows: false,
-        responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                },
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1.5,
-                    slidesToScroll: 1.5,
-                },
-            },
-            {
-                breakpoint: 529,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
     });
 
+    $('[home_slider_1]').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false,
+    });
 
 
     $('[client-testimonial]').slick({
@@ -61,10 +47,43 @@ $(document).ready(function() {
     });
 
 
+    $('[clients-slider]').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1.5,
+                slidesToScroll: 1.5,
+            },
+        },
+        {
+            breakpoint: 529,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+        ],
+    });
+
+
+
 });
 
 
-$(".passwordBtn").on("click", function() {
+$(".passwordBtn").on("click", function () {
     var passwordInput = $("#passwordInput");
     var icon = $(this).find(".password_hide");
 
@@ -80,19 +99,19 @@ $(".passwordBtn").on("click", function() {
 });
 
 
-$(".filter-open-btn").click(function() {
+$(".filter-open-btn").click(function () {
     $(".filter-sidebar").addClass("filter-sidebar-show")
     $("#overlay").show();
 })
 
 
-$("#overlay").click(function() {
+$("#overlay").click(function () {
     $(".filter-sidebar").removeClass("filter-sidebar-show")
     $(this).hide();
 })
 
 
-$(".filter-sidebar-closeBtn").click(function() {
+$(".filter-sidebar-closeBtn").click(function () {
     $(".filter-sidebar").removeClass("filter-sidebar-show")
     $("#overlay").hide();
 })
